2e5afb948380ab39a45cbed0fd288d05d6dadd16
Pavan Tumati <pavan.github@tumati.net>
2017-12-04 19:34:33 -0600

    Cosmetic -- Removed biological term jargon to make the interface seem more general

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ let {expect} = require('chai');
 let Mutator = require( '../index.js' );
 
 describe( '#Mutator', function() {
-    let chromosome = {
+    let baseObject = {
         a: 0,
         b: 0,
         c: 0,
@@ -12,14 +12,14 @@ describe( '#Mutator', function() {
     };
     
     let mutationGroup = [
-        { gene: "a", start: 5, checkRange: (i) => (i < 10 ), step: (i) => i+1 },
-        { gene: "b", start: 10, checkRange: (i) => ( i < 20 ), step: (i) => i+2 },
-        { gene: "c", start: -10, checkRange: (i) => ( i < 0 ), step: (i) => i+1 }
+        { targetKey: "a", start: 5, checkRange: (i) => (i < 10 ), step: (i) => i+1 },
+        { targetKey: "b", start: 10, checkRange: (i) => ( i < 20 ), step: (i) => i+2 },
+        { targetKey: "c", start: -10, checkRange: (i) => ( i < 0 ), step: (i) => i+1 }
     ];
 
     it( 'should leave unmutated properties untouched', function() {
         let expectedTotal = 5 * 5 * 10;
-        let mutationBag = new Mutator( chromosome, mutationGroup );
+        let mutationBag = new Mutator( baseObject, mutationGroup );
         let result = 0;
         mutationBag.mutations.forEach(element => {
             if( element.d === 'Unmutated property' ) result++;
@@ -30,7 +30,7 @@ describe( '#Mutator', function() {
 
     // TODO:  Can do better on this test
     it( 'should filter out unwanted mutations', function() {
-        let mutationBag = new Mutator( chromosome, mutationGroup, (mutation) => { 
+        let mutationBag = new Mutator( baseObject, mutationGroup, (mutation) => { 
             return( mutation.a >= 7 ) 
         });
 
